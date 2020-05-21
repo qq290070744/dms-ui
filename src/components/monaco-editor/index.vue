b07877d3b40d758eaf9ed5acec34156a4cbe8ae0
李俊楠 <junnan.li@yatsenglobal.com>
2020-05-21 11:53:26 +0800

    注册编辑器关键字提示

--- src/components/monaco-editor/index.vue
+++ src/components/monaco-editor/index.vue
@@ -6,6 +6,8 @@
 
 <script>
 import * as monaco from 'monaco-editor'
+import { language as mysqlDef } from 'monaco-editor/esm/vs/basic-languages/mysql/mysql'
+import { language as redisDef } from 'monaco-editor/esm/vs/basic-languages/redis/redis'
 import { waitRefShow } from '../../utils/util'
 export default {
   props: {
@@ -20,6 +22,12 @@ export default {
     readOnly: {
       type: Boolean,
       default: false
+    },
+    suggestions: {
+      type: Array,
+      default () {
+        return []
+      }
     }
   },
   data () {
@@ -43,12 +51,40 @@ export default {
       return this.getValue().split('\n').map(command => command.trim().replace(/\s+/g, ' ')).filter(v => !!v)
     },
     initEditor () {
+      this.registerSuggention()
       waitRefShow(this, 'target').then((ref) => {
         this.editor = monaco.editor.create(ref, {
           value: this.initialValue,
           readOnly: this.readOnly,
           language: this.language
         })
+      })
+    },
+    genSuggestion (...args) {
+      args = args.reduce((result, arg) => {
+        return result.concat(arg)
+      }, [])
+      return args.map((kw) => {
+        return {
+          label: kw,
+          kind: monaco.languages.CompletionItemKind.Function,
+          insertText: kw,
+        }
+      })
+    },
+    registerSuggention () {
+      const defs = {
+        mysql: mysqlDef,
+        redis: redisDef
+      }
+      const lang = this.language
+      const { builtinFunctions, keywords, operators } = defs[lang]
+      monaco.languages.registerCompletionItemProvider(lang, {
+        provideCompletionItems: () => {
+          return {
+            suggestions: this.genSuggestion(builtinFunctions, keywords, operators)
+          }
+        }
       })
     }
   }
