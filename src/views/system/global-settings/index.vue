f1008c733a953b3e8a7e17d633289afb0f06d18e
李俊楠 <junnan.li@yatsenglobal.com>
2020-08-14 14:35:52 +0800

    globalsettings

---
+++ src/views/system/global-settings/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <basic-container>
+    <x-form @submit="submit" :fields="fields"></x-form>
+  </basic-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fields: [
+        ['insulate_word_list', '脱敏字段', {
+          component: 'a-textarea',
+          props: {
+            placeholder: '以逗号分隔'
+          }
+        }],
+        ['max_limit', '查询最大Limit限制', {
+          component: 'a-input-number',
+          props: {
+            min: 1
+          }
+        }],
+        ['query_timeout', '查询超时时间/秒', {
+          component: 'a-input-number',
+          props: {
+            min: 30
+          }
+        }],
+      ]
+    }
+  },
+  methods: {
+    submit (values) {
+      console.log(values)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
